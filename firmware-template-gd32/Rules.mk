de0d5c39fd6aac24ac2266dbe962c38ac94653fe
Arjan <Arjan.van.Vught@gmail.com>
2026-06-05 20:56:56 +0200

    Migrate GD32 template to C++ hardfault & superloop
    Replace C hardfault handler with a C++ implementation and update build rules accordingly. Rules.mk: set fixed target name (gd32h759.bin), switch startup/hardfault build to use $(LINE) and compile hardfault_handler.cpp with the C++ compiler, update library list (add network, superloop, hal), and adjust include/link flags. common/make/gd32/Includes.mk: add FREE_RTOS detection and conditional includes for FreeRTOS or lib-superloop. Add firmware.h include to bootloader main and update .gitignore. Remove stale binary and validation/make artifacts (gd32h7xx.bin, Validate.mk, C hardfault), add lib-superloop project files and move superloop softwaretimers sources into the new lib-superloop layout. These changes prepare the GD32 firmware template for the C++ hardfault handler and default superloop-based build flow (or FreeRTOS when enabled).

--- firmware-template-gd32/Rules.mk
+++ firmware-template-gd32/Rules.mk
@@ -11,7 +11,7 @@ AR	 = $(PREFIX)ar
 BOARD?=BOARD_GD32H759I_EVAL
 ENET_PHY?=DP83848
 
-TARGET=$(FAMILY).bin
+TARGET=gd32h759.bin
 LIST=$(FAMILY).list
 MAP=$(FAMILY).map
 SIZE=$(FAMILY).size
@@ -23,19 +23,10 @@ DEFINES:=$(addprefix -D,$(DEFINES))
 
 include ../common/make/gd32/Board.mk
 include ../common/make/gd32/Mcu.mk
-include ../firmware-template/libs.mk
-include ../common/make/DmxNodeNodeType.mk
-include ../common/make/DmxNodeOutputType.mk
 include ../common/make/gd32/Includes.mk
-include ../common/make/Artnet.mk
-include ../common/make/gd32/mbedtls.mk
 include ../common/make/gd32/Validate.mk
 
-LIBS+=gd32 clib
-
-ifeq ($(findstring NODE_SHOWFILE,$(DEFINES)),NODE_SHOWFILE)
-	LIBS+=showfile
-endif
+LIBS+=network superloop hal gd32 clib
 
 # The variable for the libraries include directory
 LIBINCDIRS:=$(addprefix -I../lib-,$(LIBS))
@@ -68,10 +59,8 @@ CPPOPS+=-fno-use-cxa-atexit
 LDOPS=--gc-sections --print-gc-sections --print-memory-usage
 
 PLATFORM_LIBGCC+= -L $(shell dirname `$(CC) $(COPS) -print-libgcc-file-name`)
-PLATFORM_LIBC+= -L $(shell dirname `$(CC) $(COPS) --print-file-name=libc.a`)
 
 $(info $$PLATFORM_LIBGCC [${PLATFORM_LIBGCC}])
-$(info $$PLATFORM_LIBC [${PLATFORM_LIBC}])
 
 C_OBJECTS=$(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.c,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.c)))
 C_OBJECTS+=$(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.cpp,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.cpp)))
@@ -125,14 +114,14 @@ $(LIBDEP):
 $(BUILD_DIRS) :
 	mkdir -p $(BUILD_DIRS)
 
-$(BUILD)startup_$(FAMILY).o : $(FIRMWARE_DIR)/startup_$(FAMILY).S
-	$(AS) $(COPS) -D__ASSEMBLY__ -c $(FIRMWARE_DIR)/startup_$(FAMILY).S -o $(BUILD)startup_$(FAMILY).o
+$(BUILD)startup_$(LINE).o : $(FIRMWARE_DIR)/startup_$(LINE).S
+	$(AS) $(COPS) -D__ASSEMBLY__ -c $(FIRMWARE_DIR)/startup_$(LINE).S -o $(BUILD)startup_$(LINE).o
 
-$(BUILD)hardfault_handler.o : $(FIRMWARE_DIR)/hardfault_handler.c	
-	$(CC) $(COPS) -c $(FIRMWARE_DIR)/hardfault_handler.c -o $(BUILD)hardfault_handler.o
+$(BUILD)hardfault_handler.o : $(FIRMWARE_DIR)/hardfault_handler.cpp	
+	$(CPP) $(COPS) $(CPPOPS) -c $(FIRMWARE_DIR)/hardfault_handler.cpp -o $(BUILD)hardfault_handler.o
 
-$(BUILD)main.elf: Makefile.GD32 $(LINKER) $(BUILD)startup_$(FAMILY).o $(BUILD)hardfault_handler.o $(OBJECTS) $(LIBDEP)
-	$(LD) $(BUILD)startup_$(FAMILY).o $(BUILD)hardfault_handler.o $(OBJECTS) -Map $(MAP) -T $(LINKER) $(LDOPS) -o $(BUILD)main.elf $(LIBGD32) $(LDLIBS) $(PLATFORM_LIBGCC) -lgcc
+$(BUILD)main.elf: Makefile.GD32 $(LINKER) $(BUILD)startup_$(LINE).o $(BUILD)hardfault_handler.o $(OBJECTS) $(LIBDEP)
+	$(LD) $(BUILD)startup_$(LINE).o $(BUILD)hardfault_handler.o $(OBJECTS) -Map $(MAP) -T $(LINKER) $(LDOPS) -o $(BUILD)main.elf $(LIBGD32) $(LDLIBS) $(PLATFORM_LIBGCC) -lgcc
 	$(PREFIX)objdump -D $(BUILD)main.elf | $(PREFIX)c++filt > $(LIST)
 	$(PREFIX)size -A -x $(BUILD)main.elf
 
